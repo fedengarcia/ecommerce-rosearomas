eee56340c026cc415699c571414d5ae21c6f129e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-03-31 16:26:12 +0200

    Boostrap correccion

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/style.scss';
 import { CarouselContext } from '../context/carouselContext';
+import Script from 'next/script';
 
 
 function MyApp({ Component, pageProps }) {
   return (<CarouselContext>
   <Component {...pageProps} />
+  <Script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"/>
+  <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"/>
+
   </CarouselContext>)
 }
 
