b3bde0c0b76e727ac3f9292c53ace51dcbd4d82d
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-01 21:52:24 +0200

    Quien Soy

--- pages/QuienSoy.js
+++ pages/QuienSoy.js
@@ -2,20 +2,27 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import logoWhap from "../public/whatsapp.png";
 import Image from "next/image";
-
+import foto from "../public/fotoCara.png";
 
 function QuienSoy() {
 
 
   return (<>
     <Header />
-        <h1>Quien Soy?</h1>
+      <div className='container-bio'>
+          <div className='biografia'>
+              <Image src={foto} alt="Foto del usuario" width={100} height={100}/>
+              <h1>{"<Nombre del usuario>"}</h1>
+              <h1>{"JAJAJAJAJJAJJAJAJAJAJ"}</h1>
+              <p>DESCRIPCION DEL USUARIO</p>
+          </div>
+      </div>
 
-        <div className="container-logoWhap">
-          <a href="https://wa.me/543487513839?text=Hola Rosé! Quería hacer una consulta" target="_blank" rel="noopener">
-              <Image src={logoWhap} alt="Whap" className='logoWhap' width={80} height={80}/>
-          </a>
-        </div>
+      <div className="container-logoWhap">
+        <a href="https://wa.me/543487513839?text=Hola Rosé! Quería hacer una consulta" target="_blank" rel="noopener">
+            <Image src={logoWhap} alt="Whap" className='logoWhap' width={80} height={80}/>
+        </a>
+      </div>
     <Footer/>
     </>
   );
