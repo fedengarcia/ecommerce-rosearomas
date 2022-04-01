bc9fe4b5aa08b90660130546e896ec6a8c26d701
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-01 21:49:05 +0200

    Nuestros Productos terminado. Con responsive

--- pages/NuestrosProductos.js
+++ pages/NuestrosProductos.js
@@ -8,13 +8,46 @@ function NuestrosProductos() {
 
   return (<>
     <Header />
-        <h1>Nuestros Productos</h1>
-
-        <div className="container-logoWhap">
-          <a href="https://wa.me/543487513839?text=Hola Rosé! Quería hacer una consulta" target="_blank" rel="noopener">
-              <Image src={logoWhap} alt="Whap" className='logoWhap' width={80} height={80}/>
-          </a>
+      <div className='container-Nprods'>
+        <div className='container-Nprods-left'>
+            <img src="https://firebasestorage.googleapis.com/v0/b/rosepagina.appspot.com/o/nuestrosProductos%2Fvela.png?alt=media&token=79251fd4-5591-45cb-9322-0ebedb490feb"
+            alt="VELAS" className='fotoProd'/>
+            <div>
+                <h2>VELAS</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto eligendi quas ducimus voluptas vitae harum praesentium incidunt aspernatur enim doloremque, mollitia commodi vel maiores exercitationem quos alias at laudantium? Aliquid.</p>
+            </div>
         </div>
+        <div className='container-Nprods-right'>
+            <img src="https://firebasestorage.googleapis.com/v0/b/rosepagina.appspot.com/o/nuestrosProductos%2Fdifusor.png?alt=media&token=94fe8b7e-2860-4d7b-918c-1586d6ba0362"
+            alt="VELAS" className='fotoProd'/>
+            <div>
+                <h2>DIFUSORES</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto eligendi quas ducimus voluptas vitae harum praesentium incidunt aspernatur enim doloremque, mollitia commodi vel maiores exercitationem quos alias at laudantium? Aliquid.</p>
+            </div>
+        </div>
+        <div className='container-Nprods-left'>
+            <img src="https://firebasestorage.googleapis.com/v0/b/rosepagina.appspot.com/o/nuestrosProductos%2Fhomespray.png?alt=media&token=ee23a5c1-9ca9-4bf5-ac10-d69429939263"
+            alt="VELAS" className='fotoProd'/>
+            <div>
+                <h2>HOMESPRAYS</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto eligendi quas ducimus voluptas vitae harum praesentium incidunt aspernatur enim doloremque, mollitia commodi vel maiores exercitationem quos alias at laudantium? Aliquid.</p>
+            </div>
+        </div>
+        <div className='container-Nprods-right'>
+            <img src="https://firebasestorage.googleapis.com/v0/b/rosepagina.appspot.com/o/nuestrosProductos%2Fbombon.png?alt=media&token=58ee5f8d-a0b9-44ad-ab6a-63ec346e2be7"
+            alt="VELAS" className='fotoProd'/>
+            <div>
+                <h2>BOMBONES</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto eligendi quas ducimus voluptas vitae harum praesentium incidunt aspernatur enim doloremque, mollitia commodi vel maiores exercitationem quos alias at laudantium? Aliquid.</p>
+            </div>
+        </div>
+      </div>
+
+      <div className="container-logoWhap">
+        <a href="https://wa.me/543487513839?text=Hola Rosé! Quería hacer una consulta" target="_blank" rel="noopener">
+            <Image src={logoWhap} alt="Whap" className='logoWhap' width={80} height={80}/>
+        </a>
+      </div>
     <Footer/>
     </>
   );
