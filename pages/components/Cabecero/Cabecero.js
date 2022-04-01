59b4f980100e6bdddfd390b0453e5adb281c6008
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-01 10:34:19 -0300

    Acomodo de "cabecero"

--- pages/components/Cabecero/Cabecero.js
+++ pages/components/Cabecero/Cabecero.js
@@ -7,10 +7,14 @@ export default function Header(){
 
     return (
         <div className="cabecero" id="cabecero">
+            <h1 className='tituloMobile'>ROSÉ AROMAS</h1>
+
             <h1 className="tituloI">ROSÉ</h1>
 
-            <div>
-                <Image src={logo} alt="LOGO" width={180} height={150}/>
+            <div className='container-logoRose'>
+                <div className='container2-logoRose'>
+                    <Image src={logo} alt="LOGO" width={180} height={150}/>
+                </div>
                 <p>By Carolina</p>
             </div>
 
