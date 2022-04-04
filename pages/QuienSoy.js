460dc8aa4a2cdb62f55a689ba1d6c7359aa44934
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-04 17:44:34 -0300

    QuienSoy responsivo + googleMaps

--- pages/QuienSoy.js
+++ pages/QuienSoy.js
@@ -2,20 +2,21 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import logoWhap from "../public/whatsapp.png";
 import Image from "next/image";
-import foto from "../public/fotoCara.png";
 
 function QuienSoy() {
-
 
   return (<>
     <Header />
       <div className='container-bio'>
           <div className='biografia'>
-              <Image src={foto} alt="Foto del usuario" width={100} height={100}/>
-              <h1>{"<Nombre del usuario>"}</h1>
-              <h1>{"JAJAJAJAJJAJJAJAJAJAJ"}</h1>
-              <p>DESCRIPCION DEL USUARIO</p>
+              <h1>Carolina Magnani</h1>
+              <p>Hola! Bienvenidos! <br></br> Les voy a contar sobre mi, soy estudiante de marketing y emprendedora. Este emprendimiento nació como una forma de distenderme y hacer lo que me gusta. Espero que en los productos encuentren la relajación y bienestar que intento transmitir</p>
           </div>
+      </div>
+
+      <div className="container-map">
+        <p className="container-map-titulo">Me encuentro en:</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3303.9751660023026!2d-59.02641888506977!3d-34.09577663830547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bb0b73b7edaef3%3A0xb64dedbafcacaf5a!2sIngenieria%20%26%20Obras!5e0!3m2!1ses!2sar!4v1649103856858!5m2!1ses!2sar" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
       <div className="container-logoWhap">
