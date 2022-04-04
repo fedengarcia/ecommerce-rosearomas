03ceb4a42a014f84117838218e310d2bd49eef00
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-04 10:42:56 -0300

    Cambios en NavBar responsivo

--- pages/components/NavBar/NavBar.js
+++ pages/components/NavBar/NavBar.js
@@ -4,32 +4,31 @@ import Link from 'next/link'
 export default function NavBar() {
   
 
-    return(<div className="navbar-container">
-        <nav className="navbar navbar-expand-lg navbar-light " id='navBar'>
+    return(
+    <div className="navbar-container">
 
+        <nav className="navbar navbar-expand-custom navbar-light " id='navBar'>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span id="referenciaMenu">Incio</span>
-                <span className="navbar-toggler-icon"></span>
             </button>
 
      
             <div className="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
                 
                 <div className="navbar-nav">
-                <Link href={"/"} className="nav-link">Inicio</Link>
-                <Link href={"/QuienSoy"} className="nav-link">¿Quién soy?</Link>
-                <Link href={"/NuestrosProductos"} className="nav-link">Nuestros productos</Link>
-                <Link href={`/Tienda`} className="nav-link">Tienda</Link>
-                <Link href={"/Refill"} className="nav-link">Refill</Link>
-                <Link href='#contacto' className="nav-link">Contacto</Link>
-
+                    <Link href={"/"} className="nav-link">Inicio</Link>
+                    <Link href={"/QuienSoy"} className="nav-link">¿Quién soy?</Link>
+                    <Link href={"/NuestrosProductos"} className="nav-link">Nuestros productos</Link>
+                    <Link href={`/Tienda`} className="nav-link">Tienda</Link>
+                    <Link href={"/Refill"} className="nav-link">Refill</Link>
+                    <Link href='#contacto' className="nav-link">Contacto</Link>
                 </div>
             </div>
 
         </nav>
         <CartWidget/>
-        </div>
+    </div>
     );
 }
 
