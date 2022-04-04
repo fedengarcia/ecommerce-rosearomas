bf3140bc74ca0426400380f970e2e28b0a0d3432
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-04 17:37:53 +0200

    Tienda container

--- pages/components/TiendaContainer/TiendaContainer.js
+++ pages/components/TiendaContainer/TiendaContainer.js
@@ -5,11 +5,19 @@ import CardProducto from '../CardProducto/CardProducto'
 export default function TiendaContainer ({productos, itemType}) {
 
 
-
     return (
-        <div className="tiendaContainer">
-            <h1>PROD / {itemType}</h1>
-            {productos.map(producto => <CardProducto producto={producto} key={producto.id}></CardProducto>)}
+        <div className="tienda-container">
+            <div className="title-container">
+                <h1>{itemType}</h1>
+            </div>
+
+            <div className="tienda">
+
+                {itemType === "Todo" 
+                ? productos.map(producto => <CardProducto producto={producto} key={producto.id}></CardProducto>) 
+                :  productos.filter(prod => prod.Categoria === itemType).map(producto => <CardProducto producto={producto} key={producto.id}></CardProducto>)}
+            
+            </div>
         </div>
     )
 }
