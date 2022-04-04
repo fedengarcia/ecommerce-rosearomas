7a0f15911f0b48c819059684029cadd3a66964d5
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-04 16:09:06 +0200

    NavbarTienda

---
+++ pages/components/NavBar/NavBarTienda.js
@@ -0,0 +1,39 @@
+
+
+export default function NavBarTienda({setItemType}) {
+  
+
+    const handleClick = (type) => {
+        setItemType(type)
+    }
+
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light " id='navBar'>
+
+            <div className="navbar-tienda">
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span id="referenciaMenu">Incio</span>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+        
+                <div className="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
+                    
+                    <ul><li onClick={() =>handleClick("Todo")}>Todo</li></ul>
+                    <ul><li onClick={() =>handleClick("Velas")}>Velas</li></ul>
+                    <ul><li onClick={() =>handleClick("Homespray")}>Homespray</li></ul>
+                    <ul><li onClick={() =>handleClick("Difusores")}>Difusores</li></ul>
+                    <ul><li onClick={() =>handleClick("Bombones")}>Bombones</li></ul>
+
+                </div>
+
+            </div>
+        </nav>
+    );
+}
+
+
+
+
+
