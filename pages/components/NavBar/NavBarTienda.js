8ad2afb9012a39014c4b0b0c0812396ba6a6f28f
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-04 17:08:54 +0200

    Navbar Tienda

--- pages/components/NavBar/NavBarTienda.js
+++ pages/components/NavBar/NavBarTienda.js
@@ -8,17 +8,15 @@ export default function NavBarTienda({setItemType}) {
     }
 
     return(
-        <nav className="navbar navbar-expand-lg navbar-light " id='navBar'>
+        <nav className="navbar navbar-expand navbar-light navbar-tienda" id='navBar'>
 
-            <div className="navbar-tienda">
-
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span id="referenciaMenu">Incio</span>
+{/* 
+                <button className="navbar-toggler" id="navbar-toggler-tienda" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup2" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span id="referenciaMenu">Tipo de producto</span>
                     <span className="navbar-toggler-icon"></span>
-                </button>
-
+                </button> */}
         
-                <div className="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse justify-content-around" id="navbarNavAltMarkup2">
                     
                     <ul>
                         <li onClick={() =>handleClick("Todo")}>Todo</li>
@@ -26,11 +24,11 @@ export default function NavBarTienda({setItemType}) {
                         <li onClick={() =>handleClick("Homespray")}>Homespray</li>
                         <li onClick={() =>handleClick("Difusores")}>Difusores</li>
                         <li onClick={() =>handleClick("Bombones")}>Bombones</li>
+
                     </ul>
 
                 </div>
 
-            </div>
         </nav>
     );
 }
