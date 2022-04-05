254255dc9377a8ce9de677c7637f15759c0c415f
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-05 17:56:34 +0200

    Create ItemCount.js

---
+++ pages/components/ItemCount/ItemCount.js
@@ -0,0 +1,44 @@
+import {useState} from 'react'
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { Typography } from '@material-ui/core';
+
+
+
+
+export default function ItemCount ({stock, setAmount}) {
+    const [cantidad, setCantidad] = useState(1);
+
+
+    const handleAddItem = () => {
+        if(cantidad < stock){
+            setCantidad(cantidad + 1);
+            setAmount(cantidad + 1);
+        }
+    }
+    
+    const handleRemoveItem = () => {
+        if(cantidad > 0){
+            setCantidad(cantidad - 1);
+            setAmount(cantidad - 1);
+        }
+    }
+
+    return (
+        <div className="root">
+
+
+            <RemoveIcon className="buttonitemcount"onClick={handleRemoveItem} color="primary" style={{fontSize:"1em"}}/>
+
+
+            <div className="itemCountContainer">
+                <Typography style={{fontSize:"0.7em"}}>{`Cantidad: ${cantidad}`}</Typography>
+            </div>
+
+
+            <AddIcon className="buttonitemcount" onClick={handleAddItem} style={{fontSize:"1em"}}/>
+
+        </div>
+    );
+}
