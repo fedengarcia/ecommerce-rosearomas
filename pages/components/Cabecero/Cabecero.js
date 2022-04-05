5c3692678ea349a6b6ba1d301f603dcaa4016423
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-05 14:13:40 -0300

    CSS cabecero responsivo

--- pages/components/Cabecero/Cabecero.js
+++ pages/components/Cabecero/Cabecero.js
@@ -8,8 +8,10 @@ export default function Header(){
     return (
         <div className="cabecero" id="cabecero">
             <h1 className='tituloMobile'>ROSÉ AROMAS</h1>
-
-            <h1 className="tituloI">ROSÉ</h1>
+            
+            <div className='container-titulo'>
+                <h1 className="tituloI">ROSÉ</h1>
+            </div>
 
             <div className='container-logoRose'>
                 <div className='container2-logoRose'>
@@ -18,7 +20,9 @@ export default function Header(){
                 <p>By Carolina</p>
             </div>
 
-            <h1 className="tituloD">AROMAS</h1>
+            <div className='container-titulo'>
+                <h1 className="tituloD">AROMAS</h1>
+            </div>
         </div>
     )
 }
