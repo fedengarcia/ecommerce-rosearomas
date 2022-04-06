6ebd96ffee8c1df3db8ff4b78b7d1dc93dd3c77d
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-06 10:56:33 -0300

    Estilos de cards-prods-inicio

---
+++ pages/components/ProductosInicio/ProductosInicio.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Image from 'next/image'
+import prod1 from "../../../public/productos-inicio/vela.png"
+import prod2 from "../../../public/productos-inicio/homespray.png"
+import prod3 from "../../../public/productos-inicio/difusor.png"
+import prod4 from "../../../public/productos-inicio/bombon.png"
+import Link from 'next/link'
+
+export default function ProductosInicio(){
+
+    return(
+        <div>
+            <h2 class="titulo-productos">NUESTROS PRODUCTOS</h2>
+            <div class="contenedor-productos-inicio">
+                <Link href={"/Tienda"} class="CardProdInicio">
+                    <div class="productos-inicio">
+                        <Image src={prod1} alt="DIFUSORES"/>
+                        
+                        <h3>VELAS</h3>
+                    </div>
+                </Link>
+                <Link href={"/Tienda"}>
+                    <div class="productos-inicio">
+                        <Image src={prod2} alt="BOMBON"/>
+                        <h3>HOMESPRAY</h3>
+                    </div>
+                </Link>
+                <Link href={"/Tienda"}>
+                    <div class="productos-inicio">
+                        <Image src={prod3} alt="VELAS"/>
+                        <h3>DIFUSORES</h3>
+                    </div>
+                </Link>
+                <Link href={"/Tienda"}>
+                    <div class="productos-inicio">
+                        <Image src={prod4} alt="HOMESPRAY"/>
+                        <h3>BOMBONES DE SOJA</h3>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
+}
