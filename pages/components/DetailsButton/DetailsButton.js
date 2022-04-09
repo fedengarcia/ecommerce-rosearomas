c91ce981d4dcdcc26ecb2d848d508f6cf3deed21
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-09 00:49:03 -0300

    Boton details

---
+++ pages/components/DetailsButton/DetailsButton.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function DetailsButton(){
+
+    return(
+        <>
+            <button className="details-cart-button">DETALLES</button>
+        </>
+    )
+}
