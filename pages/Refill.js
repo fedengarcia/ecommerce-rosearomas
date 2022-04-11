6fbb757a507657ffdba9bc8c3c6d4dd63d571253
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-11 14:02:59 -0300

    Page Refill completa

--- pages/Refill.js
+++ pages/Refill.js
@@ -9,7 +9,21 @@ function Refill() {
 
   return (<>
     <Header />
-        <h1>Refill</h1>
+        <h1 className="title-refill">Refill</h1>
+
+        <div className="container-refill">
+          <p>Este servicio consiste en el rellenado de recipientes con cera de soja de origen vegetal y esencias concentradas. 
+            Es una tecnica amigable con el planeta ya que se reutilizan los envases luego de terminar la vela, y de esta forma, se reduce la cantidad de recipientes.
+          </p>
+          <p className="puntos-refill">Puntos necesarios:</p>
+          <ul>
+            <li>- Contar con un envase</li>
+            <li>- Envase resistente a alta temperatura</li>
+            <li>- Si es de madera, ser ignifugo</li>
+            <li>- Servicio único para: Zárate-Campana-Lima</li>
+          </ul>
+          <a href="https://wa.me/543487513839?text=Hola Rosé! Quería hacer una consulta sobre los Refill" target="_blank" rel="noopener" className="contacto-refill">Contactar servicio: 3487542190</a>
+        </div>
 
         <div className="container-logoWhap">
           <a href="https://wa.me/543487513839?text=Hola Rosé! Quería hacer una consulta" target="_blank" rel="noopener">
