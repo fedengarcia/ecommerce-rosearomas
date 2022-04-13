c0053a0014ea67f2e5b5b7d714b43f85f192974c
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-13 15:33:31 -0300

    carouselFragancias

--- pages/components/CarouselFragancia/CarouselFragancia.js
+++ pages/components/CarouselFragancia/CarouselFragancia.js
@@ -1,4 +1,4 @@
-import React, { useContext,useState } from "react";
+import React, { useContext,useState,useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 import  'bootstrap/dist/css/bootstrap.min.css' ;
 import left from "../Carousel/icons8-izquierda-círculo-90.png";
@@ -17,15 +17,13 @@ export default function CarouselFragancia(){
     )
 
     const {carouselFragancia,carouselFraganciaSML} = useContext(crlContext);
-    
-    const [carouselTamaño,setCarouselTamaño]=useState(carouselFragancia)
 
     return(
         <>
             <h1 className="titulo-fragancias">NUESTRAS FRAGANCIAS</h1>
             <div className="container-carouselFragancia">
-                <Carousel prevIcon={leftArrow} nextIcon={rightArrow} interval={3000}>
-                        {carouselTamaño.map((foto)=>{
+                <Carousel prevIcon={leftArrow} nextIcon={rightArrow} interval={3000} className="carouselF-1">
+                        {carouselFragancia.map((foto)=>{
                             return(
                                 <Carousel.Item key={foto.id}>
                                     <Image
@@ -33,6 +31,25 @@ export default function CarouselFragancia(){
                                     alt={foto.titulo}
                                     width={1320}
                                     height={300}
+                                    className="carouselF-foto"
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>{foto.titulo}</h3>
+                                        <p>{foto.descripcion}</p>
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            )
+                        })}
+                </Carousel>
+                <Carousel prevIcon={leftArrow} nextIcon={rightArrow} interval={3000} className="carouselF-2">
+                        {carouselFraganciaSML.map((foto)=>{
+                            return(
+                                <Carousel.Item key={foto.id}>
+                                    <Image
+                                    src={foto.img}
+                                    alt={foto.titulo}
+                                    width={700}
+                                    height={500}
                                     className="carouselF-foto"
                                     />
                                     <Carousel.Caption>
