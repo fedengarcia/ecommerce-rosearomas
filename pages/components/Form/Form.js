419a68d0f8b174653b39191487230e7ceccabd16
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-04-13 16:03:18 +0200

    Form PayerInfo

--- pages/components/Form/Form.js
+++ pages/components/Form/Form.js
@@ -1,68 +1,50 @@
 import React,{useState, useContext,useEffect} from 'react';
 import TextField from '@material-ui/core/TextField';
 import {UseCartContext} from '../../../context/CartContext';
+import MercadoPagoButton from '../MercadoPagoButton/MercadoPagoButton';
 import Link from 'next/link';
 
 
 
 
 export default function Form () {
-    const [buyerInfo, setBuyerInfo] = useState({});
+    const [payerInfo, setPayerInfo] = useState({});
 
-    const {saveBuy,order,} = useContext(UseCartContext);
-
-
-    useEffect(() => {
-        console.log(buyerInfo)
-    }, [buyerInfo]);
 
     const handleNameData = (e) => {
-        setBuyerInfo({...buyerInfo,nombre: e.target.value})
+        setPayerInfo({...payerInfo,nombre: e.target.value})
     }
 
     const handleSurnameData = (e) => {
-        setBuyerInfo({...buyerInfo,apellido: e.target.value})
+        setPayerInfo({...payerInfo,apellido: e.target.value})
     }
 
     const handleEmailData = (e) => {
-        setBuyerInfo({...buyerInfo,email: e.target.value})
+        setPayerInfo({...payerInfo,email: e.target.value})
     }
 
     const handlePhoneData = (e) => {
-        setBuyerInfo({...buyerInfo,phone: e.target.value})
+        setPayerInfo({...payerInfo,phone: e.target.value})
     }
 
     const handleCityData = (e) => {
-        setBuyerInfo({...buyerInfo,ciudad: e.target.value})
+        setPayerInfo({...payerInfo,ciudad: e.target.value})
     }
 
     const handleAddressData = (e) => {
-        setBuyerInfo({...buyerInfo,direccion: e.target.value})
+        setPayerInfo({...payerInfo,direccion: e.target.value})
     }
 
     const handleAddressNumberData = (e) => {
-        setBuyerInfo({...buyerInfo,numeroCasa: e.target.value})
+        setPayerInfo({...payerInfo,numeroCasa: e.target.value})
     }
 
     const handlePostalCodeData = (e) => {
-        setBuyerInfo({...buyerInfo,codigoPostal: e.target.value})
+        setPayerInfo({...payerInfo,codigoPostal: e.target.value})
     }
 
 
-    const handleAccept = () => {
-        var hasNumber = /\d/;   
-        // MODIFICAR
-        if(buyerInfo.nombre === "" || buyerInfo.apellido === "" || buyerInfo.email === "" || buyerInfo.phone === ""){
-            // history.push('/dialog/formDialogError');
-        }else if ( hasNumber.test(buyerInfo.nombre) || hasNumber.test(buyerInfo.apellido)) {
-            // history.push('/dialog/formDialogError');
-        } else if (!buyerInfo.email.includes("@")) {
-            // history.push('/dialog/formDialogError');
-        }else{
-            saveBuy(buyerInfo);
-            // history.push('/dialog/endBuyDialog');
-        }
-    }
+
 
     return (
     <form className="form">
@@ -85,7 +67,7 @@ export default function Form () {
 
 
         {/* MERCADO PAGO COMPRAR */}
-        <button onClick={() => handleAccept()}>Finalizar Compra</button>
+        <MercadoPagoButton payerInfo={payerInfo} />
         <Link href="/Cart" className="linkStyle"><button>Cancelar</button></Link>
 
         </form>
