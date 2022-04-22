66c9e07b29a3c46745550eb8b46aff728a22d7f3
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-22 10:11:54 -0300

    seo metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,33 @@
 import '../styles/style.scss';
 import { CarouselContext } from '../context/carouselContext';
 import {CartContext} from '../context/CartContext';
-import Script from 'next/script';
-
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+
+
+
+
   return (
-    
-    <CarouselContext>
-      <CartContext>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossOrigin="anonymous"/>
-        
-        <Component {...pageProps} />
-      </CartContext>
-    
-    </CarouselContext>
+    <>
+      <Head>
+        <title>Rosé Aromas</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta charSet="utf-8"/>
+        <meta name="description" content="Sitio web de un emprendimiento gastronómico del rubro hamburguesas. Zárate, Buenos Aires"/>
+	      <meta name="keywords" content="aromas, vela, difusor, bombon, homespray, perfume, decoracion, emprendimiento, rose"/>
+        <link rel='icon' href='/vela.png' type='image/png'/>
+      </Head>
+      <CarouselContext>
+        <CartContext>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossOrigin="anonymous"/>
+          
+          <Component {...pageProps} />
+        </CartContext>
+      
+      </CarouselContext>
+    </>
   )
 }
 
