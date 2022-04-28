572cae0550a1bac39c887bb558ca3dc4f49f7031
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-28 16:59:24 -0300

    .2

--- Context/StorageContext.js
+++ Context/StorageContext.js
@@ -13,10 +13,10 @@ export function StorageContext ({children}) {
             localStorage.setItem("FechaCarrito",JSON.stringify(data))
         }else{
             const fecha = new Date();
-            const data = localStorage.getItem("FechaCarrito");
+            const data = JSON.parse(localStorage.getItem("FechaCarrito"));
             if (fecha - data.fecha < (24 * 60 * 60 * 1000)) { // un dia en ms
                 setCarritoStorage(JSON.parse(localStorage.getItem("CarritoRoseAromas")))
-              }
+            }
         }
     },[])// eslint-disable-line react-hooks/exhaustive-deps
 
