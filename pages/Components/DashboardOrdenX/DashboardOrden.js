dddb771afc74492f931dc9136df3dd59757cdacd
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-28 10:43:36 -0300

    .

--- pages/Components/DashboardOrdenX/DashboardOrden.js
+++ pages/Components/DashboardOrdenX/DashboardOrden.js
@@ -4,7 +4,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { setOrderEntregada } from '../../../firebase/Firebase';
+import { setOrderEntregada } from '../../../firebaseX/Firebase';
 
 export default function DashboardOrden({order,actualizarOrders}) {
 
