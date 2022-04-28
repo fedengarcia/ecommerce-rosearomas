f251ab429c86fcd87edf49842ec63b1510797df7
TimoteoGambella <timi.gambella@hotmail.com>
2022-04-28 17:17:45 -0300

    storage

--- Context/StorageContext.js
+++ Context/StorageContext.js
@@ -7,14 +7,15 @@ export function StorageContext ({children}) {
     const [carritoStorage, setCarritoStorage]=useState([])
 
     useEffect(()=>{
-        const data = {'fecha': new Date()};
+        const data = {'dia': (new Date()).getDate(),'mes':((new Date()).getMonth()+1),"año":(new Date()).getFullYear()};
         if(!localStorage.getItem("CarritoRoseAromas")){
             localStorage.setItem("CarritoRoseAromas",JSON.stringify(carritoStorage))
             localStorage.setItem("FechaCarrito",JSON.stringify(data))
         }else{
-            const fecha = new Date();
+            const fecha = {'dia': (new Date()).getDate(),'mes':((new Date()).getMonth()+1),"año":(new Date()).getFullYear()};
             const data = JSON.parse(localStorage.getItem("FechaCarrito"));
-            if (fecha - data.fecha < (24 * 60 * 60 * 1000)) { // un dia en ms
+
+            if ((data.año>=fecha.año) && (data.mes>=fecha.mes) && (data.dia>=fecha.dia)) { // un dia en ms
                 setCarritoStorage(JSON.parse(localStorage.getItem("CarritoRoseAromas")))
             }
         }
