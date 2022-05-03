51230e109a78a066cfcdd7f8790f1d3dc8a922ae
TimoteoGambella <timi.gambella@hotmail.com>
2022-05-03 11:46:37 -0300

    buscador

--- pages/Components/DashboardBuscadorX/DashboardBuscador.js
+++ pages/Components/DashboardBuscadorX/DashboardBuscador.js
@@ -1,25 +1,41 @@
-export default function DashboardBuscador () {
+import { useEffect } from "react";
+
+export default function DashboardBuscador ({listaProds,setProdsFiltrados,prodsFiltrados}) {
     
+
+    const getIndex = (id) =>{
+        return prodsFiltrados.findIndex(item => item.id === id);
+    }
+
     const buscador=(e)=>{
+        var variable = 0
         if(e.target.value.length!=0){
             let busqueda = e.target.value.toUpperCase()
-            console.log("busqueda",busqueda)
-
             for(let prod of listaProds){
                 let tituloProd = prod.Nombre.toUpperCase().slice(0,busqueda.length)
-                console.log("tituloPrd",tituloProd)
-
                 if(tituloProd === busqueda){
-                    setPordFiltrados(prodsFiltrados=>[...prodsFiltrados,prod])
+                    console.log(busqueda,tituloProd)
+                    variable=1
+                    if(getIndex(prod.id) === -1){
+                        setProdsFiltrados(prodsFiltrados => [...prodsFiltrados, prod])
+                    }
                 }
             }
-            console.log(prodsFiltrados)
+        }else{
+            setProdsFiltrados([])
+        }
+        if(variable===0){
+            setProdsFiltrados([])
         }
     }
+
+    useEffect(()=>{
+        console.log(prodsFiltrados)
+    },[prodsFiltrados])
+
     return (
-        <>
-                        <input type="text" placeholder="Buscar producto" onChangeCapture={buscador}/>
-                
-        </>
+        <div>
+            <input type="text" placeholder="Buscar producto" onChangeCapture={buscador}/>
+        </div>
     )
 }
