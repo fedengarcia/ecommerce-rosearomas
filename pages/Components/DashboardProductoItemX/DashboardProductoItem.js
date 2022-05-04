6ad0e4aab8da5ff853a865661a94e14a7d22cab0
TimoteoGambella <timi.gambella@hotmail.com>
2022-05-04 20:56:15 -0300

    errores

--- pages/Components/DashboardProductoItemX/DashboardProductoItem.js
+++ pages/Components/DashboardProductoItemX/DashboardProductoItem.js
@@ -27,49 +27,49 @@ export default function DashboardProductoItem ({producto}) {
     const handleStockData = (e) => {
 
     }
-    console.log(producto)
+
     return (
         <>
             {producto===undefined?<></>:
-            <div className="dash-prod-item-container">
-                <div className="dash-prod-item">
-                    {editProp === "nombre" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="nameId"  placeholder="Nombre" type="text" onChangeCapture={handleNameData}/>
-                    : <p>{producto.Nombre}</p>}
-                    <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("nombre")}}/>
-                </div>
+                <div className="dash-prod-item-container">
+                    <div className="dash-prod-item">
+                        {editProp === "nombre" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="nameId"  placeholder="Nombre" type="text" onChangeCapture={handleNameData}/>
+                        : <p>{producto.Nombre}</p>}
+                        <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("nombre")}}/>
+                    </div>
 
-                <div className="dash-prod-item">
-                    {editProp === "descripcion" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="descripcionId"  placeholder="Descripcion" type="text" onChangeCapture={handleDescriptionData}/>
-                    : <p>{producto.Descripcion}</p>}
-                    <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("descripcion")}}/>
-                </div>
+                    <div className="dash-prod-item">
+                        {editProp === "descripcion" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="descripcionId"  placeholder="Descripcion" type="text" onChangeCapture={handleDescriptionData}/>
+                        : <p>{producto.Descripcion}</p>}
+                        <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("descripcion")}}/>
+                    </div>
 
-                <div className="dash-prod-item">
-                    {editProp === "dextra" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="dextraId"  placeholder="Datos Extras" type="text" onChangeCapture={handleDextraData}/>
-                    : <p>{producto.Dextra}</p>}
-                    <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("dextra")}}/>
-                </div>
+                    <div className="dash-prod-item">
+                        {editProp === "dextra" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="dextraId"  placeholder="Datos Extras" type="text" onChangeCapture={handleDextraData}/>
+                        : <p>{producto.Dextra}</p>}
+                        <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("dextra")}}/>
+                    </div>
 
-                <div className="dash-prod-item">
-                    {editProp === "categoria" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="categoriaId"  placeholder="Categoria" type="text" onChangeCapture={handleCategoriaData}/>
-                    : <p>{producto.Categoria}</p>}
-                    <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("categoria")}}/>
-                </div>
+                    <div className="dash-prod-item">
+                        {editProp === "categoria" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="categoriaId"  placeholder="Categoria" type="text" onChangeCapture={handleCategoriaData}/>
+                        : <p>{producto.Categoria}</p>}
+                        <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("categoria")}}/>
+                    </div>
 
 
-                <div className="dash-prod-item">
-                    {editProp === "precio" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="precioId"  placeholder="Precio" type="number" onChangeCapture={handlePriceData}/>
-                    : <p>{producto.Precio}</p>}
-                    <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("precio")}}/>
-                </div>
+                    <div className="dash-prod-item">
+                        {editProp === "precio" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="precioId"  placeholder="Precio" type="number" onChangeCapture={handlePriceData}/>
+                        : <p>{producto.Precio}</p>}
+                        <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("precio")}}/>
+                    </div>
 
-                <div className="dash-prod-item">
-                    {editProp === "stock" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="stockId"  placeholder="Stock" type="number" onChangeCapture={handleStockData}/>
-                    : <p>{producto.Stock}</p>}
-                    <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("stock")}}/>
+                    <div className="dash-prod-item">
+                        {editProp === "stock" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="stockId"  placeholder="Stock" type="number" onChangeCapture={handleStockData}/>
+                        : <p>{producto.Stock}</p>}
+                        <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("stock")}}/>
+                    </div>
+                    
                 </div>
-                
-            </div>
             }
         </>
     )
