7597bb808c2b72af8ef818a52d7a18a86144b3ab
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-05-04 16:57:42 +0200

    Productos Dashboard

--- pages/Components/DashboardProductoItemX/DashboardProductoItem.js
+++ pages/Components/DashboardProductoItemX/DashboardProductoItem.js
@@ -8,25 +8,64 @@ import TextField from '@material-ui/core/TextField';
 export default function DashboardProductoItem ({producto}) {
     const [editProp, setEditProp] = useState("");
 
-    const handleNameData = () => {
+    const handleNameData = (e) => {
 
     }
 
-    const handleCategoriaData = () => {
+    const handleDescriptionData = (e) => {
 
     }
+    const handleDextraData = (e) => {
+
+    }
+    const handleCategoriaData = (e) => {
+
+    }
+    const handlePriceData = (e) => {
+
+    }
+    const handleStockData = (e) => {
+
+    }
+    console.log(producto)
     return (
+        
         <div className="dash-prod-item-container">
             <div className="dash-prod-item">
                 {editProp === "nombre" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="nameId"  placeholder="Nombre" type="text" onChangeCapture={handleNameData}/>
-                 : <h2>{producto.Nombre}</h2>}
+                 : <p>{producto.Nombre}</p>}
                 <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("nombre")}}/>
             </div>
 
             <div className="dash-prod-item">
+                {editProp === "descripcion" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="descripcionId"  placeholder="Descripcion" type="text" onChangeCapture={handleDescriptionData}/>
+                 : <p>{producto.Descripcion}</p>}
+                <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("descripcion")}}/>
+            </div>
+
+            <div className="dash-prod-item">
+                {editProp === "dextra" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="dextraId"  placeholder="Datos Extras" type="text" onChangeCapture={handleDextraData}/>
+                 : <p>{producto.Dextra}</p>}
+                <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("dextra")}}/>
+            </div>
+
+            <div className="dash-prod-item">
                 {editProp === "categoria" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="categoriaId"  placeholder="Categoria" type="text" onChangeCapture={handleCategoriaData}/>
-                 : <h2>{producto.Categoria}</h2>}
+                 : <p>{producto.Categoria}</p>}
                 <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("categoria")}}/>
+            </div>
+
+
+            <div className="dash-prod-item">
+                {editProp === "precio" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="precioId"  placeholder="Precio" type="number" onChangeCapture={handlePriceData}/>
+                 : <p>{producto.Precio}</p>}
+                <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("precio")}}/>
+            </div>
+
+            <div className="dash-prod-item">
+                {editProp === "stock" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="stockId"  placeholder="Stock" type="number" onChangeCapture={handleStockData}/>
+                 : <p>{producto.Stock}</p>}
+                <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("stock")}}/>
             </div>
             
         </div>
