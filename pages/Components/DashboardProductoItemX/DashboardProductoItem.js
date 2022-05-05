6db260917972ac897bc27da02985cfc724be7a89
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2022-05-05 13:36:20 +0200

    Update DashboardProductoItem.js

--- pages/Components/DashboardProductoItemX/DashboardProductoItem.js
+++ pages/Components/DashboardProductoItemX/DashboardProductoItem.js
@@ -9,28 +9,16 @@ import TextField from '@material-ui/core/TextField';
 
 export default function DashboardProductoItem ({producto}) {
     const [editProp, setEditProp] = useState("");
+    const [data,setData] = useState("");
 
-    const handleNameData = (e) => {
 
+    const handleChangeData = (e) => {
+        setData(e.target.value)
     }
 
-    const handleDescriptionData = (e) => {
-
-    }
-    const handleDextraData = (e) => {
-
-    }
-    const handleCategoriaData = (e) => {
-
-    }
-    const handlePriceData = (e) => {
-
-    }
-    const handleStockData = (e) => {
-
-    }
     const handleConfirm = () => {
-        
+        console.log("TU DATO A CAMBIAR ES", data);
+        console.log("TU DATO A CAMBIAR ES", editProp)
     }
 
     return (
@@ -42,25 +30,25 @@ export default function DashboardProductoItem ({producto}) {
                 <div className='title-description-dash-prod-item dash-prod-item-box'>
                     <div>
                         <h5>Nombre: </h5>
-                        {editProp === "nombre" ? <TextField className="title-dash-prod-item" size="medium" autoComplete="off" color="secondary"  id="nameId"  placeholder="Nombre" type="text" onChangeCapture={handleNameData}/>
+                        {editProp === "nombre" ? <TextField className="title-dash-prod-item" size="medium" autoComplete="off" color="secondary"  id="nameId"  placeholder="Nombre" type="text" onChangeCapture={handleChangeData}/>
                         : <h5>{producto.Nombre}</h5>}
                         
                         {editProp !== "nombre" ? <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("nombre")}}/>
                         : <div>
                             <CheckCircleOutlineOutlinedIcon onClick={handleConfirm}/>
-                            <CancelOutlinedIcon onClick={()=> setEditProp("")}/>
+                            <CancelOutlinedIcon onClick={()=> {setEditProp(""); setData("")}}/>
                         </div>
                         } 
                     </div>
 
                     <div>
                         <h5>Descripcion: </h5>
-                        {editProp === "descripcion" ? <TextField className="form-input" multiline rows={4} size="medium" autoComplete="off" color="secondary"  id="descripcionId"  placeholder="Descripcion" type="text" onChangeCapture={handleDescriptionData}/>
+                        {editProp === "descripcion" ? <TextField className="form-input" multiline rows={4} size="medium" autoComplete="off" color="secondary"  id="descripcionId"  placeholder="Descripcion" type="text" onChangeCapture={handleChangeData}/>
                             : <h5>{producto.Descripcion}</h5>}
                         {editProp !== "descripcion" ? <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("descripcion")}}/>
                         : <div>
                             <CheckCircleOutlineOutlinedIcon onClick={handleConfirm}/>
-                            <CancelOutlinedIcon onClick={()=> setEditProp("")}/>
+                            <CancelOutlinedIcon onClick={()=> {setEditProp(""); setData("")}}/>
                             </div>
                         } 
                     </div>
@@ -71,37 +59,37 @@ export default function DashboardProductoItem ({producto}) {
 
                     <div>
                         <h5>Categoria: </h5>
-                        {editProp === "categoria" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="categoriaId"  placeholder="Categoria" type="text" onChangeCapture={handleCategoriaData}/>
+                        {editProp === "categoria" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="categoriaId"  placeholder="Categoria" type="text" onChangeCapture={handleChangeData}/>
                         : <h5>{producto.Categoria}</h5>}
 
                        {editProp !== "categoria" ? <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("categoria")}}/>
                         : <div>
                             <CheckCircleOutlineOutlinedIcon onClick={handleConfirm}/>
-                            <CancelOutlinedIcon onClick={()=> setEditProp("")}/>
+                            <CancelOutlinedIcon onClick={()=> {setEditProp(""); setData("")}}/>
                             </div>
                         } 
                     </div>
 
                     <div>
                         <h5>Precio: $ </h5>
-                        {editProp === "precio" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="precioId"  placeholder="Precio" type="number" onChangeCapture={handlePriceData}/>
+                        {editProp === "precio" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="precioId"  placeholder="Precio" type="number" onChangeCapture={handleChangeData}/>
                         : <h5>{producto.Precio}</h5>}
                         {editProp !== "precio" ? <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("precio")}}/>
                         : <div>
                             <CheckCircleOutlineOutlinedIcon onClick={handleConfirm}/>
-                            <CancelOutlinedIcon onClick={()=> setEditProp("")}/>
+                            <CancelOutlinedIcon onClick={()=> {setEditProp(""); setData("")}}/>
                             </div>
                         } 
                     </div>
 
                     <div>
                         <h5>Stock: </h5>
-                        {editProp === "stock" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="stockId"  placeholder="Stock" type="number" onChangeCapture={handleStockData}/>
+                        {editProp === "stock" ? <TextField className="form-input" size="medium" autoComplete="off" color="secondary"  id="stockId"  placeholder="Stock" type="number" onChangeCapture={handleChangeData}/>
                         : <h5>{producto.Stock}</h5>}
                         {editProp !== "stock" ? <EditTwoToneIcon fontSize="large" onClick={() => {setEditProp("stock")}}/>
                         : <div>
                             <CheckCircleOutlineOutlinedIcon onClick={handleConfirm}/>
-                            <CancelOutlinedIcon onClick={()=> setEditProp("")}/>
+                            <CancelOutlinedIcon onClick={()=> {setEditProp(""); setData("")}}/>
                             </div>
                         } 
                     </div>
